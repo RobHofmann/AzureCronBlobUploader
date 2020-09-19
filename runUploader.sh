82194654035fec7ea1e4c2ff4a4e29e9c056da4e
Rob Hofmann <rob.hofmann@HOFMANN.int>
2020-09-19 14:11:45 +0200

    added quiet

--- runUploader.sh
+++ runUploader.sh
@@ -2,4 +2,4 @@ FOLDER="/data"
 mkdir $FOLDER
 echo "Container URL: $BLOB_DESTINATIONCONTAINER_URL"
 echo "Container Key: $BLOB_DESTINATIONKEY"
-azcopy --source $FOLDER --destination $BLOB_DESTINATIONCONTAINER_URL --dest-key $BLOB_DESTINATIONKEY --recursive
+azcopy --source $FOLDER --destination $BLOB_DESTINATIONCONTAINER_URL --dest-key $BLOB_DESTINATIONKEY --recursive --quiet
